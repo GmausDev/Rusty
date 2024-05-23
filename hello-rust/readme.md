70d173ac35bf8a069f4ca8641177201fa1e31dc8
Jorge Quevedo Duran <jorge.quevedo.duran@avanade.com>
2024-05-23 17:05:18 +0200

    Add serde and tokio dependencies

---
+++ hello-rust/readme.md
@@ -0,0 +1,93 @@
+Bienvenido a este proyecto de Rust. Esta guía te ayudará a empezar con Rust, un lenguaje de programación moderno y seguro. Incluiremos pasos para la instalación, creación de un proyecto básico, y recursos adicionales para profundizar en el ecosistema de Rust.
+
+Instalación de Rust
+Para instalar Rust, necesitarás los C++ Build Tools 2013 o versiones más recientes.
+
+Instala Rust utilizando rustup:
+sh
+Copiar código
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+Verifica la instalación:
+sh
+Copiar código
+rustc --version
+Crear un Proyecto Nuevo
+Crea un nuevo proyecto llamado hello-rust:
+sh
+Copiar código
+cargo new hello-rust
+cd hello-rust
+Compila el proyecto:
+sh
+Copiar código
+cargo build
+Ejecuta el proyecto:
+sh
+Copiar código
+cargo run
+
+Juegito
+
+https://aaronerhardt.gitlab.io/crab-tag/#slow
+
+
+https://plv.mpi-sws.org/rustbelt/
+
+https://blog.logrocket.com/unsafe-rust-how-and-when-not-to-use-it/
+
+https://survey.stackoverflow.co/2023
+
+
+https://www.whitehouse.gov/wp-content/uploads/2024/02/Final-ONCD-Technical-Report.pdf
+
+Unsafe uses
+https://blog.logrocket.com/unsafe-rust-how-and-when-not-to-use-it/
+
+
+Rendimiento 
+https://www.enmilocalfunciona.io/comparativa-de-rendimiento-entre-lenguajes-de-programacion-compilados-java-golang-y-rust/
+
+https://benchmarksgame-team.pages.debian.net/benchmarksgame/index.html
+
+https://medium.com/@slimm609/programming-language-performance-comparisons-9a55abad28f4
+
+https://thenewstack.io/which-programming-languages-use-the-least-electricity/
+
+ Rust vs c++ Operaciones complejas
+
+https://medium.com/@pepitoscrespo/rust-and-c-between-blackholes-and-fractals-2c1f148cec78
+
+Os en Rust
+https://www.redox-os.org/
+
+https://rust-for-linux.com/
+
+https://www.tomshardware.com/news/rust-in-linux-kernel#:~:text=Adding%20support%20for%20Rust%20adds%20about%2012%2C500%20lines,which%20Rust%20has%20a%20certain%20amount%20of%20interoperability.
+
+https://unix.stackexchange.com/questions/768873/how-much-of-the-linux-kernel-is-currently-coded-in-rust#:~:text=so%200.03%25%20of%20the%20code,phy%20driver%20written%20in%20Rust.
+
+https://www.rust-lang.org/es/what/wasm
+
+
+Herramientas y ecosistema 
+crates.io: Rust Package Registry
+https://crates.io/
+
+
+https://www.geeksforgeeks.org/rust-vs-c/
+
+https://dev.to/mukeshkuiry/go-vs-rust-speed-test-which-one-to-choose-in-2024-1ck
+
+https://bitfieldconsulting.com/posts/rust-vs-go#:~:text=Rust%20and%20Go%20are%20both%20awesome&text=Rust%20is%20a%20low%2Dlevel,focused%20on%20safety%20and%20performance.&text=Whereas%3A,%2C%20reliable%2C%20and%20efficient%20software.
+
+https://doc.rust-lang.org/rust-by-example/
+
+https://github.com/rust-lang/rust?tab=readme-ov-file
+
+https://github.com/rust-unofficial/awesome-rust
+
+https://awesome-rust.com/
+
+
+proyectos
+https://boytacean.joao.me/
--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,3 +1,130 @@
+use std::thread;
+use std::ptr;
+
 fn main() {
     println!("Hello, world!");
+}
+
+
+
+// Rust garantiza que y es una referencia válida a x, evitando accesos a memoria no válida.
+fn secure_memory() {
+    let x = 5;
+    let y = &x; // y es una referencia a x
+    println!("El valor de y es: {}", y);
+}
+
+
+// Aunque Rust enfatiza la seguridad de la memoria, 
+// también permite usar bloques unsafe para 
+// realizar operaciones que normalmente estarían prohibidas por el compilador. 
+// Esto debe hacerse con cuidado, 
+// ya que es responsabilidad del programador garantizar la seguridad en estos casos.
+fn boom_memory() {
+    let mut x: i32 = 10;
+    let y: *mut i32 = &mut x; // y es un puntero a x
+
+    unsafe {
+        *y = 20; // Modificando x a través de y en un bloque unsafe
+    }
+
+    println!("El valor de x es: {}", x);
+}
+
+
+// Rust es capaz de manejar colecciones grandes 
+// y realizar operaciones sobre ellas rápidamente.
+fn vectorcitos() {
+    let mut vec: Vec<i32> = Vec::with_capacity(1000);
+    for i in 0..1000 {
+        vec.push(i);
+    }
+    let sum: i32 = vec.iter().sum();
+    println!("La suma de los elementos del vector es: {}", sum);
+}
+
+
+
+// Rust facilita la programación concurrente, 
+// lo que permite ejecutar tareas en paralelo para mejorar el rendimiento.
+
+
+fn hilitos() {
+    let handles: Vec<_> = (0..10).map(|i| {
+        thread::spawn(move || {
+            println!("Hilo número: {}", i);
+        })
+    }).collect();
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+}
+
+
+// Utilizamos un bucle para crear 10 hilos. Cada hilo imprime su número (del 0 al 9).
+// La función thread::spawn crea un nuevo hilo, ejecutando el código que se pasa dentro del move closure. 
+// El move garantiza que el hilo tome posesión de cualquier variable utilizada dentro del closure.
+
+fn concurrencia_segura() {
+    let handles: Vec<_> = (0..10).map(|i| {
+        thread::spawn(move || {
+            println!("Hilo número: {}", i);
+        })
+    }).collect();
+
+// Los manejadores de los hilos (handles) se recogen en un vector.
+// Después de crear los hilos, el programa espera a que todos terminen su ejecución utilizando handle.join().unwrap(). 
+// Esto asegura que el programa principal no termine antes de que los hilos completen su trabajo.
+    for handle in handles {
+        handle.join().unwrap();
+    }
+}
+
+
+
+
+fn puntero_loco() {
+    let mut x = 5;
+    // convierte una referencia mutable a x en un puntero crudo mutable (*mut i32).
+    let raw = &mut x as *mut i32;
+    // El bloque unsafe permite realizar operaciones que el compilador normalmente no permitiría por motivos de seguridad.
+    // *raw = 10; modifica el valor apuntado por raw directamente, cambiando x a 10.
+    unsafe {
+        *raw = 10;
+    }
+    println!("El valor de x es: {}", x);
+}
+
+
+// fn divide(dividendo: f64, divisor: f64) -> Option<f64> define una función que retorna un Option<f64>.
+// Si el divisor es 0.0, retorna None, indicando una división inválida.
+// De lo contrario, retorna Some(dividendo / divisor), proporcionando el resultado de la división.
+fn divide(dividendo: f64, divisor: f64) -> Option<f64> {
+    if divisor == 0.0 {
+        None
+    } else {
+        Some(dividendo / divisor)
+    }
+}
+
+// La función main llama a divide(10.0, 0.0) y usa una expresión match para manejar el resultado.
+// Si la división es válida (Some(result)), imprime el resultado.
+// Si es inválida (None), imprime un mensaje de error indicando división por cero.
+fn robusto_y_fuerte() {
+    match divide(10.0, 0.0) {
+        Some(result) => println!("Resultado: {}", result),
+        None => println!("Error: División por cero"),
+    }
+}
+
+
+
+
+fn sum(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+fn main() {
+    println!("La suma es: {}", sum(2, 3));
 }
